07c479d2c14858a559e64b2b8b04ffdff437120f
Djumabaevs <djumabaevb@gmail.com>
2023-02-01 15:19:10 +0600

    failed appl se sing in

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -3,8 +3,10 @@ export type AmplifyDependentResourcesAttributes = {
     "memrflutterd4649faa": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
