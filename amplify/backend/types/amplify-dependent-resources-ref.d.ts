c461fb55c9562bac14aff327b8e15ce91c011d65
Djumabaevs <djumabaevb@gmail.com>
2023-02-01 12:51:54 +0600

    pulled from aws backend via cli

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "memrflutterd4649faa": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
